ff3d740cdf2dfad407fa90c0353ab5b2e012dc71
MarcusQuintana <marcusquintana7889@gmail.com>
2024-09-18 16:18:53 -0400

    test

---
+++ src/weatherbutton.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Button  ({ radar })  {
+  return (
+    <button>
+      {radar}
+    </button>
+  );
+};
+
+
+
+
+<button><img src="https://www.freepik.com/free-photos-vectors/weather-app-logo" alt="my image" onClick={this.myfunction} /></button>
+
+export default Button;
